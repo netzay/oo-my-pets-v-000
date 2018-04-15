209ce132e5243c360b7bd815fb3a8c93a41e95e0
Ally Netzel <netzelally@gmail.com>
2018-04-15 03:07:32 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -85,7 +85,7 @@ class Owner
   end
   
   def list_pets
-    return "I have #{:fishes} fish, #{:dogs} dog(s), and #{:cats} cat(s)."
+    return "I have #{:fishes}.count, #{:dogs} dog(s), and #{:cats} cat(s)."
   end
 
 end
